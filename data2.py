361cf7494da7ba1184745c5a182964af48959249
samrmn <87390999+samrmn@users.noreply.github.com>
2021-10-24 23:07:34 +0200

    Update data2.py

--- data2.py
+++ data2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+#data: https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Umwelt/Abfallwirtschaft/Tabellen/liste-deponien.html;jsessionid=0FB686343155EDB888675D818413704B.live731
 data = pd.read_csv('C:\\Users\\Samuel\\Desktop\\zerodata\\Collected_used_sales_packaging.txt', sep=',',
                    encoding='unicode_escape', decimal='.')
 
